e26422a4020482d79730889dda3010fc858aac90
MrTrukiny <carloseduardo.tkn@gmail.com>
2023-05-26 12:29:32 -0500

    CND-S06: read planets from kepler data using fs module

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('done');
+  });
+// parse();
